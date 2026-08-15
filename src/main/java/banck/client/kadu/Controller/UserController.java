fd79eaf1a6b52f70a09180c4939e83e74ce0160a
kadu <lisboasnery@gmail.com>
2026-08-15 14:43:39 -0300

    Criando as funçoes do usuario

--- src/main/java/banck/client/kadu/Controller/UserController.java
+++ src/main/java/banck/client/kadu/Controller/UserController.java
@@ -1,4 +1,19 @@
 package banck.client.kadu.Controller;
 
+
+import banck.client.kadu.services.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+    private final UserService user;
+
+    public UserController(UserService user){
+        this.user = user;
+    }
+    public void login(String userName, String password, char cpf, String email){
+
+    }
 }
--- src/main/java/banck/client/kadu/services/UserService.java
+++ src/main/java/banck/client/kadu/services/UserService.java
@@ -1,4 +1,27 @@
 package banck.client.kadu.services;
 
 public class UserService {
+    // Criar funcoes para que quando o usuario chamar a requisicao
+    //chamar a funcao, que no caso e o login
+    private String userName;
+    private String userPassword;
+    private String email;
+    private char cpf;
+
+    public String getName(){
+        return userName;
+    }
+    public String getPassword(){
+        return userPassword;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public char getCpf(){
+        return cpf;
+    }
+
+   
+
+
 }
