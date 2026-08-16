a29976beaf9c08f1aee540f06490bd53b41ea4b0
kaduzin <joaomatheusoliveirajohn@gmail.com>
2026-08-16 12:22:57 -0300

    Create interface with AI

--- src/main/java/banck/client/kadu/Controller/UserController.java
+++ src/main/java/banck/client/kadu/Controller/UserController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    @Autowired
     private UserService userService;
 
     @PostMapping("/login")
